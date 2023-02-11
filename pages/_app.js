19228bbe2bcf8788872c31d494b4ad0819314b9b
googol88 <ronaldfleung@gmail.com>
2023-02-10 21:04:58 -0500

    feat:add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,17 @@ import '../components/accordion.css'
 import Meta from '../components/meta'
 import theme from '../lib/theme'
 import { ThemeProvider } from 'theme-ui'
-
-
+import { Analytics } from '@vercel/analytics/react';
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Meta />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Meta />
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Analytics />
+    </>
   )
 }
 
